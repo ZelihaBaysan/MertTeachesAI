ad6cb0dcb34e6e9afd02fe30d123216289cc86df
ZelihaBaysan <zelihabaysan@posta.mu.edu.tr>
2024-03-20 17:56:13 +0300

    yabamıyom

---
+++ soru5.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+m=100
+x_rand = np.random.rand(m) * 2
+y_rand = np.random.rand(m) * 4
+x=np.linspace(0,2,m)
+y=x**2
+
+fitrenin_alti = (y_rand)1

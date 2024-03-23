5d4ffabb40cda62bd12ae5aee611efb59ce1b12d
ZelihaBaysan <zelihabaysan@posta.mu.edu.tr>
2024-03-23 10:47:04 +0300

    pek anlamadım ya :(

--- soru5.py
+++ soru5.py
@@ -1,10 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-m=100
+m = 10000
 x_rand = np.random.rand(m) * 2
 y_rand = np.random.rand(m) * 4
-x=np.linspace(0,2,m)
-y=x**2
+x = np.linspace(0, 3, m)
+y = x**2
+under_filter = (y_rand < x_rand**2)
+under_dot = len(x_rand[under_filter])
+area = (under_dot * 8) / m
 
-fitrenin_alti = (y_rand)1
+plt.plot(x, y, "r-")
+plt.scatter(x_rand, y_rand, s=1)
+plt.scatter(x_rand[under_filter], y_rand[under_filter], s=1)
+plt.axvline(x=2, color="b", linestyle="--")
+plt.axhline(y=4, color="r", linestyle="--")
+plt.show()
+
+print("analitik area: ", 8 / 3)
+print(f"under area: {area}")
